1fc602661dfde9e0bbf40fecfdb57cea9d133bf0
Paulo Ricardo Horta Grando <paulorhg@hotmail.com>
2021-04-12 23:27:17 -0300

    Update index.js

--- src/views/Reserve/index.js
+++ src/views/Reserve/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, Button } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 
 import { AntDesign } from '@expo/vector-icons';
@@ -14,23 +14,27 @@ export default function Reserve() {
         navigation.navigate('Reserve');
     }
 
+    function navigateToDetails(){
+        navigation.navigate('Detalhes');
+    }
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
+                <TouchableOpacity 
+                    onPress={() => navigateToDetails()}
+                    style={styles.returnButton}
+                >
+                    <AntDesign name="left" size={24} color="#fff" />
+                </TouchableOpacity>
                 <Text style={styles.textHeader}>Nome Restaurante</Text>
             </View>
             <View style={styles.infos}>
                 <AntDesign name="calendar" size={24} />
                 <Text style={styles.text}>Texto Descrição</Text>
-                <Text style={styles.titleText}>Avaliação:</Text>
-                <Text style={styles.titleText}>Endereço</Text>
-                <Text style={styles.text}>texto endereço</Text>
-                <Text style={styles.titleText}>Telefone</Text>
-                <Text style={styles.text}>texto telefone</Text>
-
+                <AntDesign name="clockcircleo" size={24} />
+                <Text style={styles.text}>Texto Descrição</Text>
                 <Button title="Reservar" onPress={() => navigateToReserve()}></Button>
-
             </View>
         </View>
     );
