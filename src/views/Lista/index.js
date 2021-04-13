05ad742fbf074678729becdb9cb5b75ef5a9640f
Paulo Ricardo Horta Grando <paulorhg@hotmail.com>
2021-04-12 23:26:02 -0300

    Update index.js

--- src/views/Lista/index.js
+++ src/views/Lista/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, FlatList, SafeAreaView} from 'react-native';
 import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from '@expo/vector-icons';
 
 import styles from "./styles";
 
 export default function Lista() {
 
     const navigation = useNavigation();
+
+    function navigateToCategoria(){
+        navigation.navigate('Categoria');
+    }
 
     function navigateToDetails(){
         navigation.navigate('Detalhes');
@@ -18,6 +23,12 @@ export default function Lista() {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
+                <TouchableOpacity 
+                    onPress={() => navigateToCategoria()}
+                    style={styles.returnButton}
+                >
+                    <AntDesign name="left" size={24} color="#fff" />
+                </TouchableOpacity>
                 <Text style={styles.textHeader}>LUGARES</Text>
             </View>
 
