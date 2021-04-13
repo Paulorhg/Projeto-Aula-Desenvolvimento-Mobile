9c1f38375f4eaeada2d0c2c6cecce8b59169f659
Paulo Ricardo Horta Grando <paulorhg@hotmail.com>
2021-04-12 23:24:01 -0300

    Update styles.js

--- src/views/City/styles.js
+++ src/views/City/styles.js
@@ -6,18 +6,32 @@ export default StyleSheet.create({
 
     container: {
         flex: 1,
-        alignItems: "center",
     },
 
     header: {
         alignSelf: "stretch",
         paddingVertical: 60,
         backgroundColor: "#EF7676",
+        alignItems: "center",
+    },
+
+    textHeader: {
+        fontSize: 30,
+        fontWeight: "bold"
     },
 
     content:{
         flex: 1,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "space-around"
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "bold"
+    },
+
+    button: {
+        padding: 50
     }
 });
